e251533647760ea80e42cbf8f0609ea6d7160fe9
synthousa <r.jericho0586@gmail.com>
2024-02-27 11:22:09 +0800

    user_profile & post_item added

--- database/seeders/PermissionSeeder.php
+++ database/seeders/PermissionSeeder.php
@@ -12,18 +12,23 @@ class PermissionSeeder extends Seeder {
 
         $permissions = [
             'user_access',
+            'user_profile',
             'user_create',
             'user_update',
             'user_delete',
+
             'role_access',
             'role_create',
             'role_update',
             'role_delete',
+
             'permission_access',
             'permission_create',
             'permission_update',
             'permission_delete',
+
             'post_access',
+            'post_item',
             'post_create',
             'post_update',
             'post_delete',
