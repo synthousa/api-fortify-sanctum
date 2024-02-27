5e216d1c5e64516494a2a7801e73adcee10a5811
synthousa <r.jericho0586@gmail.com>
2024-02-27 09:10:38 +0800

    admin routes added

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -20,6 +20,7 @@ class RouteServiceProvider extends ServiceProvider {
         $this->routes(function () {
             Route::middleware('api') -> prefix('api/v1') -> group(base_path('routes/api.php'));
             Route::middleware('web') -> group(base_path('routes/web.php'));
+            Route::middleware('admin') -> prefix('admin') -> namespace($this -> namespace) -> group(base_path('routes/admin.php'));
         });
     }
 }
