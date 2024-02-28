ca976b018788571db14531ebce0406071a879b77
synthousa <r.jericho0586@gmail.com>
2024-02-28 14:21:10 +0800

    production entry

---
+++ app/Models/Admin.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Spatie\Permission\Traits\HasRoles;
+
+class Admin extends Authenticatable {
+    
+    use HasApiTokens, HasRoles, HasFactory, Notifiable;
+
+    protected $guard_name = 'admin';
+
+    protected $fillable = [
+        'name',
+        'email',
+        'password',
+    ];
+
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+        'password' => 'hashed',
+    ];
+
+}
