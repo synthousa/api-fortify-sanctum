9b60455548baeacb65d122c2d9a5fb0e35cf7385
synthousa <r.jericho0586@gmail.com>
2024-02-28 16:13:25 +0800

    refactor some code

--- routes/api.php
+++ routes/api.php
@@ -3,50 +3,40 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Auth\LogoutController;
-use Laravel\Fortify\Http\Controllers\{
-    AuthenticatedSessionController, 
-    RegisteredUserController, 
-    PasswordResetLinkController, 
-    ProfileInformationController, 
-    PasswordController
-};
+use App\Http\Controllers\Admin\{UserController, RoleController, PostController };
+use Laravel\Fortify\Http\Controllers\{AuthenticatedSessionController, RegisteredUserController, PasswordResetLinkController, ProfileInformationController, PasswordController};
 
-Route::group(['prefix' => 'auth', 'as' => 'auth.', 'middleware' => ['auth:api']], function() {
+Route::group(['middleware' => ['auth:api']], function() {
 
-    Route::withoutMiddleware(['auth:api']) -> group(function () {
-        $limit = config('fortify.limiters.login');
+    Route::prefix('auth')->group(function () {
 
-        Route::post('login', [AuthenticatedSessionController::class, 'store'])
-            -> middleware(array_filter(['guest:'.config('fortify.guard'), $limit ? 'throttle:'.$limit : null]));
+        Route::withoutMiddleware(['auth:api']) -> group(function () {
+            $limit = config('fortify.limiters.login');
 
-        Route::post('register', [RegisteredUserController::class, 'store']) 
-            -> middleware('guest:'.config('fortify.guard'));
+            Route::post('login', [AuthenticatedSessionController::class, 'store'])
+                -> middleware(array_filter(['guest:'.config('fortify.guard'), $limit ? 'throttle:'.$limit : null]));
 
-        Route::post('forgot-password', [PasswordResetLinkController::class, 'store'])
-            -> middleware('guest:'.config('fortify.guard'))
-            -> name('password.email');
+            Route::post('register', [RegisteredUserController::class, 'store']) 
+                -> middleware('guest:'.config('fortify.guard'));
+
+            Route::post('forgot-password', [PasswordResetLinkController::class, 'store'])
+                -> middleware('guest:'.config('fortify.guard'))
+                -> name('password.email');
+        });
+
+        Route::middleware('verified') -> group(function () {
+
+            Route::get('profile', fn (Request $request) => $request -> user()) -> name('user');
+            Route::put('profile', [ProfileInformationController::class, 'update']);
+            Route::put('change-password', [PasswordController::class, 'update']);
+        });
+
+        Route::post('logout', [LogoutController::class, 'destroy']);
     });
 
-    Route::post('logout', [LogoutController::class, 'destroy']);
-
-    Route::prefix('user') -> middleware('verified') -> group(function () {
-
-        Route::get('profile', fn (Request $request) => $request -> user()) -> name('user');
-
-        Route::put('profile', [ProfileInformationController::class, 'update']);
-
-        Route::put('password', [PasswordController::class, 'update']);
+    Route::prefix('admin')->group(function () {
+        Route::resource('users', UserController::class);
+        Route::resource('roles', RoleController::class);
+        Route::resource('posts', PostController::class);
     });
-
-    
 });
-
-
-// Route::middleware(['auth:api', 'role:system']) -> group(function () {
-    
-//     Route::get('/admin', function () {
-//         return response() -> json(['message' => 'hi Admin']);
-//     });
-// });
-
-
