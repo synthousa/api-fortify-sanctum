e334473362bf919d90830d31b86a827cc54190cf
synthousa <r.jericho0586@gmail.com>
2024-02-29 22:41:12 +0800

    minor fix

--- app/Http/Controllers/Admin/PostController.php
+++ app/Http/Controllers/Admin/PostController.php
@@ -13,7 +13,7 @@ class PostController extends Controller {
     }
 
     public function index(Request $request) {
-        $data = Article::latest()->paginate(10);
+        $data = Article::latest()->paginate(69);
 
         return $request -> wantsJson()
             ? response() -> json(['list' => $data], 200)
@@ -22,30 +22,62 @@ class PostController extends Controller {
     }
 
     public function create() {
-
-        //
+        return view('posts.create');
     }
 
     public function store(Request $request) {
+        $articleAttributes = request() -> validate([
+            'title' => ['required', 'max:99'],
+            'slug' => ['required', 'max:99'],
+            'img' => ['required', 'max:99'],
+            'img_desc' => ['required', 'max:999'],
+            'body' => ['required', 'max:16037'],
+            'user_id' => ['max:5'],
+        ]);
+        
+        Article::create([
+            'title'->$articleAttributes['title'],
+            'slug'->$articleAttributes['slug'],
+            'img'->$articleAttributes['img'],
+            'img_desc'->$articleAttributes['img_desc'],
+            'body'->$articleAttributes['body'],
+            'user_id' => $request->user()->id,
+        ]);
 
-        //
+        return $request->wantsJson()
+            ? response()->json(['message' => 'article created'], 201)
+            : redirect()->route('posts.index');
     }
 
-    public function show(string $id) {
-        //
+    public function show(Article $article) {
+        return view('posts.show', compact('article'));
     }
 
-    public function edit(string $id) {
-        //
+    public function edit(Article $article) {
+        return view('posts.edit', compact('article'));
     }
 
     public function update(Request $request, string $id) {
+        $articleAttributes = request() -> validate([
+            'title' => ['required', 'max:99'],
+            'slug' => ['required', 'max:99'],
+            'img' => ['required', 'max:99'],
+            'img_desc' => ['required', 'max:999'],
+            'body' => ['required', 'max:16037'],
+        ]);
+        
+        $posts-> $product->update($articleAttributes);
 
-        //
+        return $request->wantsJson()
+            ? response()->json(['message' => 'article updated'], 202)
+            : redirect()->route('posts.index');
     }
 
-    public function destroy(string $id) {
+    public function destroy(Article $article) {
+        $article->delete();
 
-        //
+        return $request->wantsJson()
+            ? response()->json(['message' => 'article deleted'], 200)
+            : redirect()->route('users.index');
     }
 }
